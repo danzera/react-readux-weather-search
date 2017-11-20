346806752f23899135aa64e3553e6848b33c8f56
Dan Zera <dczera@gmail.com>
2017-11-19 22:28:15 -0600

    flesh out form/inputs for <SearchBar /> component

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+// import action creator
 import { weatherSearch } from '../actions/';
 
 class SearchBar extends Component {
 	render() {
 		return (
-			<div>
+			<form className="input-group">
 				<input placeholder="Search for a city" />
-			</div>
+				<span className="input-group-btn">
+					<button className="btn btn-secondary" type="submit">Search</button>
+				</span>
+			</form>
 		)
 	}
 }
