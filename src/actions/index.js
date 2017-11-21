9570aba0d295e4c3e6b7555fecf7316c27063dad
Dan Zera <dczera@gmail.com>
2017-11-21 02:09:13 -0600

    include axios for making AJAX requests from our action creator, form base url for the request and pass the request in the action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,31 @@
+import axios from 'axios';
+const API_KEY = 'c844d2ab8676e10f2d45e46f217266cd';
+// root url for our weather API search
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// exporting our action type as a variable helps keep our action types consistent
+// introduces the possibility for uncaught action type mismatches between action creators and reducers
+// when we directly define the action type as a string in both the action creator and the reducer
+// this way we can import the action type variable into our reducer and know that it is referencing the same string as our action creator
+export const WEATHER_SEARCH = 'WEATHER_SEARCH';
+
 export function weatherSearch(city) {
-	const API_KEY = 'c844d2ab8676e10f2d45e46f217266cd';
-	
+	// forming the url for our API request using the base url and 
+	// NOTE: "us" at the end of the query string is the country code, just making it static for this application
+	// -- would need another variable if we wanted to be able to search for weather in other countries
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	// action to be sent from our action creator to our reducers
 	return {
-		type: 'WEATHER_SEARCH',
-		payload: city
+		// again, factoring out our action type to a variable to keep it consistent between our action creators and reducers
+		type: WEATHER_SEARCH,
+		payload: request
 	};
 }
+
+// getting a 404 error when attempting to test the request 
+// axios.get(`${ROOT_URL}&q=minneapolis,us`).then(function(res) {
+// 	console.log(res);
+// }).catch(function(err) {
+// 	console.log(err);
+// });
