3db5e7c7f074f3b501d7fe839022b21264bea6b8
Dan Zera <dczera@gmail.com>
2017-11-25 17:45:11 -0600

    condense map function calls down to a single line and omit separate callback function, clearer and more concise this way

--- src/components/CityListItem.js
+++ src/components/CityListItem.js
@@ -3,13 +3,13 @@ import Chart from './Chart';
 
 
 export default ({ cityData }) => {
-	const temperatureArray = getArray(cityData.list, 'temp');
-	const pressureArray = getArray(cityData.list, 'pressure');
-	const humidityArray = getArray(cityData.list, 'humidity');
-	console.log('cityData received', cityData);
-	console.log('temp array', temperatureArray);
-	console.log('pressure array', pressureArray);
-	console.log('humidity array', humidityArray);
+	// more ES6 conciseness
+	// if an arrow function only has a return statement you can omit the word "return"
+	// this means we can condense our map function to one line as done here
+	// we use an arrow function inside the .map() and the data to the right of => is what is returned to the new array
+	const temperatureArray = cityData.list.map(dataSnapshot => Math.round((dataSnapshot.main.temp * 9 / 5) - 459.67));
+	const pressureArray = cityData.list.map(dataSnapshot => dataSnapshot.main.pressure);
+	const humidityArray = cityData.list.map(dataSnapshot => dataSnapshot.main.humidity);
 
 	return (
 		<tr>
@@ -19,14 +19,4 @@ export default ({ cityData }) => {
 			<td><Chart data={humidityArray} color="red" /></td>
 		</tr>
 	);
-}
-
-function getArray(list, dataType) {
-	return list.map((dataSnapshot, index, list) => {
-		if (dataType === 'temp') {
-			return Math.round((dataSnapshot.main[dataType] * 9 / 5) - 459.67);
-		} else {
-			return dataSnapshot.main[dataType];
-		}	
-	});
 }
