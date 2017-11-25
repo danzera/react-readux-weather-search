a40e00eb89b6781d4ea9bf978142d019fd4b1020
Dan Zera <dczera@gmail.com>
2017-11-25 01:12:49 -0600

    change Chart.js to use arrow function for consistency and add top-level <div> to <Chart /> component for later use

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 
-export default function({ data, color }) {
+export default ({ data, color }) => {
 	return (
-		<Sparklines data={data} height={120} width={180}>
-			<SparklinesLine color={color} />
-		</Sparklines>
+		<div>
+			<Sparklines data={data} height={120} width={180}>
+				<SparklinesLine color={color} />
+			</Sparklines>
+		</div>
 	);
 }
