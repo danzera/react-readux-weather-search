02c97822ba7df54b7bb60019c7f3c51daef264cf
Dan Zera <dczera@gmail.com>
2017-11-26 00:12:21 -0600

    include average line and amount for chart references

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,11 +1,18 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+	return _.round(_.sum(data) / data.length);
+}
 
 export default ({ data, color }) => {
 	return (
 		<div>
 			<Sparklines data={data} height={120} width={180}>
 				<SparklinesLine color={color} />
+				<SparklinesReferenceLine type="avg" />
+				<div>Average: {average(data)}</div>
 			</Sparklines>
 		</div>
 	);
